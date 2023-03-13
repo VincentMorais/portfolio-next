4f64fb176b80d129eb0c57dc82e6c3d2df426d93
unknown <vincent.morais2001@outlook.fr>
2023-03-13 17:38:07 +0100

    maj timeline

--- src/components/freeze.tsx
+++ src/components/freeze.tsx
@@ -1,61 +1,32 @@
 import React from 'react';
-import { motion } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
+import { Chrono } from 'react-chrono';
 
-const Frise: React.FC = () => {
-  const years = [
-    { year: '2020', text: 'J\'ai commencé mon cursus universitaire en informatique' },
-    { year: '2022', text: 'J\'ai effectué un stage en développement web et mobile' },
-    { year: '2023', text: 'J\'ai travaillé sur un projet de recherche sur l\'apprentissage automatique' },
-  ];
+const educationItems = [
+  {
+    title: 'Baccalauréat',
+    cardTitle: 'Baccalauréat en sciences',
+    cardSubtitle: '2016 - 2018',
+    cardDetailedText: 'J\'ai obtenu mon baccalauréat en sciences de l\'Université de Paris.',
+  },
+  {
+    title: 'Licence',
+    cardTitle: 'Licence en informatique',
+    cardSubtitle: '2018 - 2021',
+    cardDetailedText: 'J\'ai étudié l\'informatique à l\'Université de Paris.',
+  },
+  {
+    title: 'Master',
+    cardTitle: 'Master en informatique',
+    cardSubtitle: '2021 - présent',
+    cardDetailedText: 'Je suis actuellement en train de poursuivre mes études en informatique à l\'Université de Paris.',
+  },
+];
 
-  const containerVariants = {
-    hidden: {},
-    visible: {
-      transition: {
-        staggerChildren: 0.2,
-      },
-    },
-  };
-
-  const yearVariants = {
-    hidden: { opacity: 0, y: -50 },
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: { duration: 0.5, ease: 'easeOut' },
-    },
-  };
-
-  const [ref, inView] = useInView({
-    rootMargin: '-50px 0px',
-    triggerOnce: true,
-  });
-
+const Frise = () => {
   return (
-    <motion.div className="frise-container" variants={containerVariants} initial="hidden" animate={inView ? "visible" : "hidden"} ref={ref}>
-      {years.map(({ year, text }, index) => (
-        <motion.div
-          key={year}
-          className="frise-year"
-          variants={yearVariants}
-        >
-          {year}
-          <motion.div className="frise-text">
-            {text}
-          </motion.div>
-          {index < years.length - 1 && (
-            <motion.div
-              className="frise-line"
-              color='white'
-              initial={{ width: 150 }}
-              animate={{ width: '150px' }}
-              transition={{ duration: 0.5, ease: 'easeOut' }}
-            />
-          )}
-        </motion.div>
-      ))}
-    </motion.div>
+    <div style={{ width: '100%', height: '500px' }}>
+      <Chrono items={educationItems} />
+    </div>
   );
 };
 
