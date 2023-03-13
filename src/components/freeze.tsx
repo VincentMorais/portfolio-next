70cbbcd06df6bbfdaab860f47fb425a08d55e308
VincentMorais <72997022+VincentMorais@users.noreply.github.com>
2023-03-13 22:05:24 +0100

    maj font size

--- src/components/freeze.tsx
+++ src/components/freeze.tsx
@@ -1,32 +1,71 @@
 import React from 'react';
-import { Chrono } from 'react-chrono';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { FaBriefcase, FaUniversity } from 'react-icons/fa';
 
-const educationItems = [
+
+interface TimelineData {
+  title: string;
+  date: string;
+  description: string;
+  icon: React.ReactNode;
+  className?: string;
+}
+
+const timelineData: TimelineData[] = [
   {
-    title: 'Baccalauréat',
-    cardTitle: 'Baccalauréat en sciences',
-    cardSubtitle: '2016 - 2018',
-    cardDetailedText: 'J\'ai obtenu mon baccalauréat en sciences de l\'Université de Paris.',
+    title: 'Premier événement',
+    date: 'Janvier 2022',
+    description: 'Ceci est une description du premier événement.',
+    icon: <FaBriefcase />,
+    className: 'vertical-timeline-element--work',
   },
   {
-    title: 'Licence',
-    cardTitle: 'Licence en informatique',
-    cardSubtitle: '2018 - 2021',
-    cardDetailedText: 'J\'ai étudié l\'informatique à l\'Université de Paris.',
+    title: 'Deuxième événement',
+    date: 'Mars 2022',
+    description: 'Ceci est une description du deuxième événement.',
+    icon: <FaUniversity />,
+    className: 'vertical-timeline-element--education',
   },
   {
-    title: 'Master',
-    cardTitle: 'Master en informatique',
-    cardSubtitle: '2021 - présent',
-    cardDetailedText: 'Je suis actuellement en train de poursuivre mes études en informatique à l\'Université de Paris.',
+    title: 'Deuxième événement',
+    date: 'Mars 2022',
+    description: 'Ceci est une description du deuxième événement.',
+    icon: <FaUniversity />,
+    className: 'vertical-timeline-element--education',
   },
+  {
+    title: 'Deuxième événement',
+    date: 'Mars 2022',
+    description: 'Ceci est une description du deuxième événement.',
+    icon: <FaUniversity />,
+    className: 'vertical-timeline-element--education',
+  },
+  {
+    title: 'Deuxième événement',
+    date: 'Mars 2022',
+    description: 'Ceci est une description du deuxième événement.',
+    icon: <FaUniversity />,
+    className: 'vertical-timeline-element--education',
+  },
+  // Ajoutez plus d'événements ici
 ];
 
 const Frise = () => {
   return (
-    <div style={{ width: '100%', height: '500px' }}>
-      <Chrono items={educationItems} />
-    </div>
+    <VerticalTimeline>
+      {timelineData.map((data, index) => (
+        <VerticalTimelineElement
+          key={index}
+          date={data.date}
+          icon={data.icon}
+          className={`vertical-timeline-element ${data.className}`}
+        >
+          <h3 className="vertical-timeline-element-title">{data.title}</h3>
+          <p>{data.description}</p>
+        </VerticalTimelineElement>
+      ))}
+    </VerticalTimeline>
   );
 };
 
