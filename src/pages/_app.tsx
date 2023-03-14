94307d524904b552a00aeab77971672e97cf96e5
VincentMorais <72997022+VincentMorais@users.noreply.github.com>
2023-03-14 14:59:10 +0100

    app debug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,48 +2,35 @@ import "../styles/style.scss";
 import "../styles/tools.scss";
 import "../styles/work.scss";
 import "../styles/police.css";
-import "../styles/card.scss"
-import "../styles/projet.scss"
-import "../styles/navbar.scss"
-import "../styles/alter.scss"
-import "../styles/rocketbutton.scss"
-import "../styles/freeze.scss"
+import "../styles/card.scss";
+import "../styles/projet.scss";
+import "../styles/navbar.scss";
+import "../styles/alter.scss";
+import "../styles/rocketbutton.scss";
+import "../styles/freeze.scss";
 
-import LoadingPage from "@/components/chargement";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { StrictMode, useEffect, useState } from "react";
 
-
 const App = ({ Component, pageProps }: AppProps) => {
   const [showLoadingPage, setShowLoadingPage] = useState(true);
+
   useEffect(() => {
-    let devEnv = process && process.env.NODE_ENV === "development";
+    const devEnv = process?.env?.NODE_ENV === "development";
     if (!devEnv) {
-      window.addEventListener("DOMContentLoaded", function () {
+      window.addEventListener("DOMContentLoaded", () => {
         navigator.serviceWorker.register("../../arc-sw.js").then(
-          function (registration) {
+          (registration) => {
             console.log("Arc service worker successful with scope: ", registration.scope);
           },
-          function (err) {
+          (err) => {
             console.log("Arc service worker registration failed: ", err);
           }
         );
       });
     }
   }, []);
-  
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowLoadingPage(false);
-    }, 15000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (showLoadingPage) {
-    return <LoadingPage />;
-  }
 
   return <Component {...pageProps} />;
 };
